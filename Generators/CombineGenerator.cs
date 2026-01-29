d0435a399cecbff17768070135e1715b7252c329
Marc Selis <marc.selis@sdworx.com>
2026-01-29 16:19:51 +0100

    Fix build

--- Generators/CombineGenerator.cs
+++ Generators/CombineGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxKind;
